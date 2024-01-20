52d7507e9ee0cbb431141294e1789facd46f0fee
zeusjke <zeus.cccp@gmail.com>
2024-01-20 04:21:16 +0300

    added virt-mangaer install script

--- arch-aur.sh
+++ arch-aur.sh
@@ -1,12 +1,16 @@
+#!/bin/bash
+
 #git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
 git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si --noconfirm
 
 echo "gnome(1) or kde(2)"
 read de
 if [ $de == 1 ]; then 
-    paru -S --noconfirm mattermost-desktop-bin spotify timeshift-autosnap portproton extension-manager resources
+    paru -S --noconfirm mattermost-desktop-bin spotify timeshift-autosnap portproton extension-manager
+    sudo flatpak install -y  net.nokyan.Resources
 elif [ $de == 2 ]; then 
     paru -S --noconfirm mattermost-desktop-bin spotify timeshift-autosnap portproton 
+    sudo pacman -S xdg-desktop-portal-gtk
 fi
 
 printf "Done!"
---
+++ virt-manager.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo pacman -S virt-manager qemu-desktop freerdp
+systemctl enable libvirtd
