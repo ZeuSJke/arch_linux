165418cd3e43247c9c76e01983c65164c17b9ffd
zeusjke <zeus.cccp@gmail.com>
2024-01-22 21:07:47 +0300

    added libappimage, changed code to vscodium and added flatpak priority in gnome

--- arch-gnome.sh
+++ arch-gnome.sh
@@ -5,7 +5,7 @@ sudo timedatectl set-local-rtc 1
 sudo hwclock --systohc
 
 sudo pacman -Sy
-sudo pacman -S --noconfirm gnome firefox gnome-tweaks mangohud papirus-icon-theme telegram-desktop discord code fragments 
+sudo pacman -S --noconfirm gnome firefox gnome-tweaks mangohud papirus-icon-theme telegram-desktop discord 
 sudo pacman -Rs --noconfirm gnome-tour epiphany gnome-maps gnome-text-editor simple-scan gnome-user-docs yelp gnome-system-monitor
 
 sudo systemctl enable gdm
--- arch-kde.sh
+++ arch-kde.sh
@@ -6,7 +6,7 @@ sudo hwclock --systohc
 
 sudo pacman -Sy
 sudo pacman -S --noconfirm plasma plasma-wayland-session 
-sudo pacman -S --noconfirm telegram-desktop discord code firefox konsole dolphin ark partitionmanager filelight ktorrent spectacle gwenview papirus-icon-theme
+sudo pacman -S --noconfirm telegram-desktop discord firefox konsole dolphin ark partitionmanager filelight ktorrent spectacle gwenview papirus-icon-theme
 
 sudo systemctl enable sddm
 
