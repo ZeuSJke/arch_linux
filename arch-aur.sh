540c96b41adca1731ab50155e7b3d15edc3ed73d
zeusjke <zeus.cccp@gmail.com>
2024-01-23 11:57:23 +0300

    fixed arch-aur.sh

--- arch-aur.sh
+++ arch-aur.sh
@@ -5,13 +5,9 @@ git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si --noconfi
 echo "gnome(1) or kde(2)"
 read de
 if [ $de == 1 ]; then 
-    paru -S --noconfirm mattermost-desktop-bin spotify timeshift-autosnap portproton extension-manager vscodium-bin 
-    sudo flatpak install -y net.nokyan.Resources
-    sudo flatpak install -y com.github.tchx84.Flatseal
-    sudo flatpak install -y de.haeckerfelix.Fragments
+    paru -S --noconfirm mattermost-desktop-bin spotify timeshift-autosnap portproton vscodium-bin 
 elif [ $de == 2 ]; then 
     paru -S --noconfirm mattermost-desktop-bin spotify timeshift-autosnap portproton vscodium-bin 
-    sudo pacman -S xdg-desktop-portal-gtk
 fi
 
 printf "Done!"
